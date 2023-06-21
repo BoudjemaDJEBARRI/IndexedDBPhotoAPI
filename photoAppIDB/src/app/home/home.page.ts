82406437690f52c94cc0194385690ccd7d02b262
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-06-21 13:16:55 +0200

    V1

---
+++ photoAppIDB/src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { PhotoService } from '../photo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  imageSource: string | undefined;
+
+  constructor(private photoService: PhotoService) {}
+
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt
+    });
+    this.imageSource = image.webPath;
+
+    this.photoService.savePhoto(image);
+  }
+}
---
+++ photoAppIDB/src/app/photo.service.ts
@@ -0,0 +1,54 @@
+
+import { Injectable } from '@angular/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Capacitor } from '@capacitor/core';
+import { Photo } from './photo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  async savePhoto(photo: Photo) {
+    const fileName = new Date().getTime() + '.jpeg';
+
+    let savedFile;
+    if (Capacitor.getPlatform() === 'web') {
+      savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: photo.webPath,
+        directory: Directory.Data
+      });
+    } else {
+      const base64Data = await this.readAsBase64(photo);
+      savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data
+      });
+    }
+
+    // Handle savedFile
+    console.log('Photo saved:', savedFile.uri);
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob);
+  }
+
+  private convertBlobToBase64(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onloadend = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject(new Error('Failed to convert blob to base64'));
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+}
