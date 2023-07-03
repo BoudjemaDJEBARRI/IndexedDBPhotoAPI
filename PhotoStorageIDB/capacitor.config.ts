fd2ee2fd7b47a59909d267b6e2cd366247c40e14
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-07-03 11:38:51 +0200

    Commit V2

---
+++ PhotoStorageIDB/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PhotoStorageIDB',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
